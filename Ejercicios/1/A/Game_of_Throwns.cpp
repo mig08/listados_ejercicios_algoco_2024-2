d4255f1aa0c39463aecc2b47c8bfd7d46ce41da3
mig08 <129887837+mig08@users.noreply.github.com>
2024-08-13 01:28:24 -0400

    Add files via upload

---
+++ Ejercicios/1/A/Game_of_Throwns.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> pila;
+
+    int cant_estudiantes, cant_comandos; 
+
+    cin >> cant_estudiantes;
+    cin >> cant_comandos;
+    
+
+return 0;
+}
